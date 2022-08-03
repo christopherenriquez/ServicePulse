898c5170a5b50514cfcf1b157544821d73bded71
awright18 <awright18@gmail.com>
2022-08-03 07:35:54 -0500

    removing files that aren't needed

--- src/ServicePulse.Blazor/Program.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ServicePulse.Blazor.Pages;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.RootComponents.RegisterCustomElement<Sample>("my-sample");
-
-await builder.Build().RunAsync();
