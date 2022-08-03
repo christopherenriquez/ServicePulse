9c3eabec3d939536445c949194771702ce351924
awright18 <awright18@gmail.com>
2022-08-03 11:22:29 -0500

    Got Blazor Custom Elements working with Razor

--- src/ServicePulse.Blazor/Program.cs
+++ src/ServicePulse.Blazor/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ServicePulse.Blazor;
+using ServicePulse.Blazor.Pages;
+using ServicePulse.Blazor.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.RootComponents.RegisterCustomElement<NavigationMenu>("sp-navmenu");
+builder.RootComponents.RegisterCustomElement<Footer>("sp-footer");
+builder.RootComponents.RegisterCustomElement<Heartbeat>("my-heartbeat");
 
 await builder.Build().RunAsync();
